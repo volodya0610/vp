53ac6cf243199a0627bce19b80f1c899043e1f8a
volodya0610 <42110265+volodya0610@users.noreply.github.com>
2026-01-11 03:50:07 +0300

    Fix awk regex in wg-add

--- scripts/wg-add.sh
+++ scripts/wg-add.sh
@@ -47,13 +47,11 @@ if [[ -z "$WG_PEER_ALLOWED_IPS" ]]; then
       split($0, items, ",");
       for (i in items) {
         gsub(/^[ \t]+|[ \t]+$/, "", items[i]);
-        if (items[i] ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\/[0-9]+$/) {
+        if (items[i] ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\/32$/) {
           split(items[i], ipcidr, "/");
-          if (ipcidr[2] == 32) {
-            val = ip_to_int(ipcidr[1]);
-            if (val > max) {
-              max = val;
-            }
+          val = ip_to_int(ipcidr[1]);
+          if (val > max) {
+            max = val;
           }
         }
       }
