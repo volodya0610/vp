91696edc8a6477a92602b8808f3b27cd8310e783
volodya0610 <42110265+volodya0610@users.noreply.github.com>
2026-01-11 03:49:44 +0300

    Relax awk regex for AllowedIPs

--- scripts/wg-add.sh
+++ scripts/wg-add.sh
@@ -47,11 +47,13 @@ if [[ -z "$WG_PEER_ALLOWED_IPS" ]]; then
       split($0, items, ",");
       for (i in items) {
         gsub(/^[ \t]+|[ \t]+$/, "", items[i]);
-        if (items[i] ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\/32$/) {
+        if (items[i] ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\/[0-9]+$/) {
           split(items[i], ipcidr, "/");
-          val = ip_to_int(ipcidr[1]);
-          if (val > max) {
-            max = val;
+          if (ipcidr[2] == 32) {
+            val = ip_to_int(ipcidr[1]);
+            if (val > max) {
+              max = val;
+            }
           }
         }
       }
